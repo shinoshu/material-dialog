9b7fe13c5bdc42507753888d3783b8b55f56e31e
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-10-17 19:21:22 +0900

    update DialogOverviewExampleDialogComponent

--- src/app/dialog-overview-example-dialog/dialog-overview-example-dialog.component.ts
+++ src/app/dialog-overview-example-dialog/dialog-overview-example-dialog.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-dialog-overview-example-dialog',
   templateUrl: './dialog-overview-example-dialog.component.html',
   styleUrls: ['./dialog-overview-example-dialog.component.css']
 })
-export class DialogOverviewExampleDialogComponent implements OnInit {
+export class DialogOverviewExampleDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  onNoClick(): void {
+    this.dialogRef.close();
   }
-
 }
