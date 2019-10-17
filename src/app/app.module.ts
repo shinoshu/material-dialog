f3531b2b9c44cdd3edbb732da102fe37784af5f1
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-10-17 19:05:44 +0900

    ng g c dialog-overview-example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DialogOverviewExampleComponent } from './dialog-overview-example/dialog-overview-example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogOverviewExampleComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dialog-overview-example/dialog-overview-example.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-overview-example',
+  templateUrl: './dialog-overview-example.component.html',
+  styleUrls: ['./dialog-overview-example.component.css']
+})
+export class DialogOverviewExampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
