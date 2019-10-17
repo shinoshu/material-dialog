da71cd3265661565d51b362d4ca0e0c47c20b104
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-10-17 19:18:18 +0900

    update DialogOverviewExampleComponent

--- src/app/dialog-overview-example/dialog-overview-example.component.ts
+++ src/app/dialog-overview-example/dialog-overview-example.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-dialog-overview-example',
   templateUrl: './dialog-overview-example.component.html',
   styleUrls: ['./dialog-overview-example.component.css']
 })
-export class DialogOverviewExampleComponent implements OnInit {
+export class DialogOverviewExampleComponent {
+  animal: string;
+  name: string;
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
 
-  ngOnInit() {
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250px',
+      data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
   }
-
 }
