2f4d314301abe9ff80c8da3533095b5a4b1f76ac
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-10-17 19:34:51 +0900

    update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DialogOverviewExampleComponent } from './dialog-overview-example/dialog-overview-example.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DialogOverviewExampleComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
